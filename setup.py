d02d73ff581214450649bafa5156cde24f34356a
Vojtěch Tomas <tomas@vojtatom.cz>
2021-11-25 21:45:03 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 setup(
     name="metaworkspace",
     packages=find_packages("."),
-    version="0.1.1",
+    version="0.1.2",
     description="Python tool simplyfing work with Metacity Modules",
     long_description=README,
     long_description_content_type="text/markdown",
