0a3e0ea3ceca3974f7d29856c5dd8ba2c0f14043
Vojta Tomas <tomas@vojtatom.cz>
2023-02-21 14:02:05 +0100

    Initial config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'bananagl/bananagl.ts'),
+            name: 'MetacityGL',
+            // the proper extensions will be added
+            fileName: 'metacitygl',
+        },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: ['react', 'react-dom', 'three', 'axios'],
+            output: {
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    react: 'React',
+                    //what about react-dom?
+                },
+            },
+        },
+    },
+    plugins: [dts()],
+});
