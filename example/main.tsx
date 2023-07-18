c8719bc7f84b05a66712eca517807ba9a7ef5711
Vojta Tomas <hello@vojtatom.cz>
2023-07-18 11:45:36 +0200

    Adding examples

---
+++ example/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { ErrorPage } from './error';
+import { DemoBasic } from './demoBasic';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <DemoBasic />,
+        errorElement: <ErrorPage />,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     build: {
@@ -8,7 +9,21 @@ export default defineConfig({
             name: 'bananagl',
             fileName: 'bananagl',
         },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: ['react', 'react-dom'],
+            output: {
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    react: 'React',
+                    //what about react-dom?
+                },
+            },
+        },
     },
+    plugins: [react()],
     resolve: {
         alias: {
             '@bananagl': resolve(__dirname, './bananagl'),
